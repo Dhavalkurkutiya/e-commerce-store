5e7731997065408e32d0998653a521500e322fe2
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-01-09 00:01:30 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Urbanist } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
+
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Store',
+  description: 'Store - The place for all your purchases.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Analytics />
+        <Footer />
+      </body>
+    </html>
+  )
+}
